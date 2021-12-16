a32c527201fe61189075fbf9652e6cdf257892de
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2021-12-16 22:48:55 +0530

    * Added couple of implementation methods for Array revers e problem statement. * Downgraded Jdk level to 8 and added surefire plugin to detect test classes as part of mvn test phase. * Added gitignore file to weed out unnecessary files

---
+++ src/main/java/com/self/practice/arrays/ArrayReverse.java
@@ -0,0 +1,46 @@
+package com.self.practice.arrays;
+
+import java.util.Optional;
+
+/**
+ * Problem Statement : Reverse an array in place.
+ * <p>
+ * Example:
+ * Input : arr[] = {1, 2, 3}       --->    Output : arr[] = {3, 2, 1}
+ * <p>
+ * Input :  arr[] = {4, 5, 1, 2}    --->    Output : arr[] = {2, 1, 5, 4}
+ * For invalid inputs (nulls), throw InvalidArgumentException
+ * <p>
+ * Bonus: Implement it using generics to pass any datatype array
+ * <p>
+ * <p>
+ * Note: Comment about the time and space complexities as comments over your
+ * methods
+ */
+
+
+public class ArrayReverse {
+
+
+    public String naiveSolution(String input) {
+
+        return Optional.ofNullable(input).map(s -> {
+            StringBuilder sb = new StringBuilder();
+            for (int i = input.length(); i > 0; i--) {
+                sb.append(input.substring(i - 1, i));
+            }
+            return sb.toString();
+        }).orElseThrow(() -> new IllegalArgumentException("Please enter non-null string"));
+    }
+
+    public String stringBuilderSolution(String input) {
+
+        return Optional.ofNullable(input).map(StringBuilder::new)
+                .map(StringBuilder::reverse)
+                .orElseThrow(IllegalArgumentException::new)
+                .toString();
+
+    }
+
+
+}
