14d2f138b674d264bcf4f1c7379a884886a21bf3
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2021-12-18 13:04:32 +0530

    Added few solutions to some Array problems

--- src/test/java/com/self/practice/arrays/SortedSquaredArrayTest.java
+++ src/test/java/com/self/practice/arrays/SortedSquaredArrayTest.java
@@ -1,6 +1,7 @@
 package com.self.practice.arrays;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,4 +13,15 @@ class SortedSquaredArrayTest {
         testObject = new SortedSquaredArray();
     }
 
+    @Test
+    void test_naiveSolution() {
+        assertArrayEquals(testObject.naiveSolution(new int[]{-4, -1, 0, 3, 10}),
+                new int[]{0, 1, 9, 16, 100});
+        assertArrayEquals(testObject.naiveSolution(new int[]{-4, -1, 0, 3, 10}),
+                new int[]{0, 1, 9, 16, 100});
+        assertArrayEquals(testObject.naiveSolution(new int[]{0, 0, 1, 2, 3}),
+                new int[]{0, 0, 1, 4, 9});
+        assertArrayEquals(testObject.naiveSolution(new int[]{-4, -1}),
+                new int[]{1, 16});
+    }
 }
--- src/test/java/com/self/practice/arrays/ValidSubsetTest.java
+++ src/test/java/com/self/practice/arrays/ValidSubsetTest.java
@@ -1,6 +1,7 @@
 package com.self.practice.arrays;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +15,18 @@ class ValidSubsetTest {
     }
 
 
+    @Test
+    void test_naiveSolution() {
+        assertTrue(testObject.naiveSolution(new int[]{5, 1, 22, 25, 6, -1, 8, 10}, new int[]{5, 1, 22, 25, 6, -1, 8, 10}));
+        assertTrue(testObject.naiveSolution(new int[]{5, 1, 22, 25, 6, -1, 8, 10}, new int[]{5, 1, 22, 6, -1, 8, 10}));
+        assertFalse(testObject.naiveSolution(new int[]{5, 1, 22, 25, 6, -1, 8, 10}, new int[]{1, 5, 10}));
+        assertFalse(testObject.naiveSolution(new int[]{5, 1, 22, 25, 6, -1, 8, 10}, new int[]{5, 1, 22, 22, 25, 6, -1, 8, 10}));
+        assertTrue(testObject.naiveSolution(new int[]{10, 10, 10}, new int[]{10, 10}));
+        assertFalse(testObject.naiveSolution(new int[]{10, 20, 30}, new int[]{50}));
+        assertFalse(testObject.naiveSolution(new int[]{10}, new int[]{10, 10}));
+        assertTrue(testObject.naiveSolution(new int[]{5, 1, 22, 25, 6, -1, 8, 10}, new int[]{1, 6, -1, 10}));
+        assertFalse(testObject.naiveSolution(new int[]{1, 5, 2, 3, 5, 2}, new int[]{1, 1, 2}));
+        assertTrue(testObject.naiveSolution(new int[]{1, 1, 2, 3, 5, 7}, new int[]{1, 1, 2}));
+
+    }
 }
