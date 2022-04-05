921c3a78ff6c3b733dda739943a356a9b9eed9db
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-04-05 22:04:16 +0530

    Updated RotateArray

--- src/main/java/com/self/practice/arrays/RotateArray.java
+++ src/main/java/com/self/practice/arrays/RotateArray.java
@@ -1,18 +1,19 @@
 package com.self.practice.arrays;
 
-import com.self.exception.NotYetImplementedException;
-
-import java.util.Arrays;
-
 /**
- * Problem Statement : Given an unsorted array <b>arr</b> of size N, rotate it by D elements (clockwise).
+ * Problem Statement : Given an unsorted array <b>arr</b> of size N,
+ * rotate it by D elements (clockwise).
  * <p>
- * Input : arr = [1, 2, 3, 4, 5], D = 2     Output : [3, 4, 5, 1, 2]
- * Input : arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], D = 3    Output : [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
+ * Input : arr = [1, 2, 3, 4, 5], D = 2
+ * Output : [3, 4, 5, 1, 2]
+ * <p>
+ * Input : arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], D = 3
+ * Output : [8, 10, 12, 14, 16, 18, 20, 2, 4, 6]
+ * <p>
+ * Note: This is not usual array rotation but a cyclical rotation clockwise
  */
 public class RotateArray {
 
-    //TODO bruteforce solution
     public int[] naiveSolution(int[] arr, int D) {
         //0, 1, 2, 3, 4     //  5
         //2, 3, 4, 5, 6
