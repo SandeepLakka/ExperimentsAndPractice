6a68f371147b2752d012932a9de201ab3822b76e
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-04-13 22:31:36 +0530

    Updated ModMax problem with TCs with better impls

--- src/main/java/com/self/practice/arrays/ModMax.java
+++ src/main/java/com/self/practice/arrays/ModMax.java
@@ -1,6 +1,9 @@
 package com.self.practice.arrays;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ModMax {
 
@@ -13,6 +16,30 @@ public class ModMax {
             }
         }
         return result;
+    }
+
+    //better
+    public int solveBetter(List<Integer> A) {
+        Set<Integer> sortedA = A.stream().sorted().collect(Collectors.toCollection(LinkedHashSet::new));
+        if (sortedA.size() <= 1) {
+            return 0;
+        } else {
+            return sortedA.stream().skip(sortedA.size() - 2).limit(1).findFirst().orElse(0);
+        }
+    }
+
+    //better than better
+    public int solveBetterer(List<Integer> A) {
+        int highest, higher;
+        highest = higher = Integer.MIN_VALUE;
+        for (int val : A) {
+            if (val > highest) {
+                highest = val;
+            } else if (val > higher && val != highest) {
+                higher = val;
+            }
+        }
+        return higher == Integer.MIN_VALUE ? 0 : higher;
     }
 
 }
--- src/test/java/com/self/practice/arrays/ModMaxTest.java
+++ src/test/java/com/self/practice/arrays/ModMaxTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,5 +25,43 @@ class ModMaxTest {
 
         list = Arrays.asList(2, 6, 4);
         assertEquals(4, underTest.solve(list));
+
+        list = Collections.singletonList(1);
+        assertEquals(0, underTest.solveBetter(list));
+
+        list = Arrays.asList(3, 3, 3);
+        assertEquals(0, underTest.solveBetter(list));
     }
+
+    @Test
+    void test_solveBetter() {
+        List<Integer> list = Arrays.asList(1, 2, 44, 3);
+        assertEquals(3, underTest.solveBetter(list));
+
+        list = Arrays.asList(2, 6, 4);
+        assertEquals(4, underTest.solveBetter(list));
+
+        list = Collections.singletonList(1);
+        assertEquals(0, underTest.solveBetter(list));
+
+        list = Arrays.asList(3, 3, 3);
+        assertEquals(0, underTest.solveBetter(list));
+    }
+
+    @Test
+    void test_solveBetterer() {
+        List<Integer> list = Arrays.asList(1, 2, 44, 3);
+        assertEquals(3, underTest.solveBetterer(list));
+
+        list = Arrays.asList(2, 6, 4);
+        assertEquals(4, underTest.solveBetterer(list));
+
+        list = Collections.singletonList(1);
+        assertEquals(0, underTest.solveBetterer(list));
+
+        list = Arrays.asList(3, 3, 3);
+        assertEquals(0, underTest.solveBetterer(list));
+    }
+
+
 }
